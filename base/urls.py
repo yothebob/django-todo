5f37863cad4b7afa34ad52d8149c2c5360d9f294
yothebob <b.cmoderndesigns@gmail.com>
2021-09-13 21:54:24 -0700

    added login logout

--- base/urls.py
+++ base/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import TaskList,  TaskDetail, TaskCreate, TaskUpdate, TaskDelete
+from .views import TaskList,  TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView
 from .models import Task
-
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+     path('login/', CustomLoginView.as_view(), name='login'),
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('create-task/', TaskCreate.as_view(), name='create-task'),
