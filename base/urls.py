810914b73522d65e486f47e9b9c9dcd037671f3d
yothebob <b.cmoderndesigns@gmail.com>
2021-09-13 20:49:38 -0700

    added view feeature, create new task feature

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import TaskList
+from .views import TaskList,  TaskDetail, TaskCreate
 from .models import Task
 
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+     path('create-task/', TaskCreate.as_view(), name='task-create'),
     ]
--- base/views.py
+++ base/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Task
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+
 
 # Create your views here.
 class TaskList(ListView):
     model = Task
+    context_object_name = "tasks"
+ 
+ 
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = "task" 
+    template_name = "base/task_detail.html"
     
+    
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
